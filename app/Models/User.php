8cfa4cb43311d4d540bc5e3d12a1eeda0f71c28e
Mitchel Pawirodinomo <pawiromitchel@hotmail.com>
2016-12-22 11:15:53 -0300

    Added local bootstrap + jquery

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-	protected $fillable = ['name', 'email', 'password'];
+	protected $fillable = [
+		'name', 
+		'email', 
+		'password'
+	];
 
 	public function setPassword($password)
 	{
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,24 +23,35 @@ class ComposerAutoloaderInitc93383e17e3c990882ba99806f2f0119
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitc93383e17e3c990882ba99806f2f0119', 'loadClassLoader'));
 
-        $map = require __DIR__ . '/autoload_namespaces.php';
-        foreach ($map as $namespace => $path) {
-            $loader->set($namespace, $path);
-        }
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        if ($useStaticLoader) {
+            require_once __DIR__ . '/autoload_static.php';
 
-        $map = require __DIR__ . '/autoload_psr4.php';
-        foreach ($map as $namespace => $path) {
-            $loader->setPsr4($namespace, $path);
-        }
+            call_user_func(\Composer\Autoload\ComposerStaticInitc93383e17e3c990882ba99806f2f0119::getInitializer($loader));
+        } else {
+            $map = require __DIR__ . '/autoload_namespaces.php';
+            foreach ($map as $namespace => $path) {
+                $loader->set($namespace, $path);
+            }
 
-        $classMap = require __DIR__ . '/autoload_classmap.php';
-        if ($classMap) {
-            $loader->addClassMap($classMap);
+            $map = require __DIR__ . '/autoload_psr4.php';
+            foreach ($map as $namespace => $path) {
+                $loader->setPsr4($namespace, $path);
+            }
+
+            $classMap = require __DIR__ . '/autoload_classmap.php';
+            if ($classMap) {
+                $loader->addClassMap($classMap);
+            }
         }
 
         $loader->register(true);
 
-        $includeFiles = require __DIR__ . '/autoload_files.php';
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInitc93383e17e3c990882ba99806f2f0119::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
         foreach ($includeFiles as $fileIdentifier => $file) {
             composerRequirec93383e17e3c990882ba99806f2f0119($fileIdentifier, $file);
         }
